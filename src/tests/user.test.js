46a158a3ec5fe4c63906275f7e722a6052c2ea93
Denys Pugachov <pugachovdenys@gmail.com>
2023-04-02 22:15:33 +0200

    143:test: add test for user should not login with wrong data

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,11 +1,38 @@
 const request = require("supertest")
 const app = require("../app")
+const User = require("../models/userModel")
+
+const userOne = {
+    name: "userOne",
+    email: "userOne@gmail.com",
+    password: "userOne123"
+}
+
+beforeEach(async () => {
+    console.log(' before Each:>> ');
+    await User.deleteMany({})
+    await new User(userOne).save()
+})
 
 test("Should signup new user", async () => {
     await request(app).post("/users").send({
-        name: "Den2",
-        email: "den2@gmail.com",
-        password: "Den1213"
+        name: "Den",
+        email: "den@gmail.com",
+        password: "Den123"
     }).expect(201)
+})
+
+test("Should loging existing user", async () => {
+    await request(app).post("/users/login").send({
+        email: userOne.email,
+        password: userOne.password,
+    }).expect(200)
+})
+
+test("Should not loging with bad user data", async () => {
+    await request(app).post("/users/login").send({
+        email: userOne.email,
+        password: "WrongPassword"
+    }).expect(400)
 })
 
