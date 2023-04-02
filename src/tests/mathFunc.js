5cb1293fccf0102ceb0feb01aadf2a61e8fe24c8
Denys Pugachov <pugachovdenys@gmail.com>
2023-04-02 21:06:36 +0200

    _140:tests: add test mmol->dl

--- src/tests/mathFunc.js
+++ src/tests/mathFunc.js
@@ -14,20 +14,42 @@ const asyncDivide = (a, b) => {
                 return rej("Numbers must be non-negative!")
             }
             res(a - b)
-        },1000)
+        }, 1000)
     })
 }
 
 const getName = () => {
     return new Promise((resolve, reject) => {
-    
+
         resolve("Den")
     })
+}
+
+const calcTip = (sum, tip = 0.25) => sum + (sum * tip)
+
+const fahrenheitToCelsius = temp => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = temp => {
+    return (temp * 1.8) + 32
+}
+
+const mmolToDl = mmol => +(mmol * 18).toFixed(0)
+
+const dlToMmol = dl => {
+    const mmolRounded = (dl / 18).toFixed(1)
+    return +mmolRounded
 }
 
 module.exports = {
     add,
     asyncDivide,
-    getName
+    getName,
+    calcTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit,
+    dlToMmol,
+    mmolToDl
 }
 
